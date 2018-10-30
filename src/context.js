7d429296d9bd2db53b8c42cf299ec8cd46f97e0d
Muhammed Mujahid <fakharm@hotmail.com>
2018-10-30 01:09:27 -0400

    added mount life cycle hook

--- src/context.js
+++ src/context.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: "abc" } },
-      { track: { track_name: "123" } }
-    ],
+    track_list: [],
     heading: "Top 10 tracks"
   };
+
+  componentDidMount() {
+    axios
+      .get("")
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
